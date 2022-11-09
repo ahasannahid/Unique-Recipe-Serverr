ba7679f90473191bd852b5ef329571b3e39eae94
ahasannahid <ahasan15-9072@diu.edu.bd>
2022-11-09 15:33:37 +0600

    create details api

--- index.js
+++ index.js
@@ -11,13 +11,36 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.qbh9oi5.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qbh9oi5.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
+        const recipeCollection = client.db('unique-recipe').collection('recipe');
+        // const orderCollection = client.db('geniousCar').collection('orders');
         
+        app.get('/recipes', async (req, res) => {
+            const query = {};
+            const cursor = recipeCollection.find(query);
+            const recipe = await cursor.toArray();
+            res.send(recipe);
+        });
+
+        app.get('/recipe', async (req, res) => {
+            const query = {};
+            const cursor = recipeCollection.find(query);
+            const recipe = await cursor.limit(3).toArray();
+            res.send(recipe);
+        });
+
+        app.get('/recipes/:id', async(req,res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const recipeDetails = await recipeCollection.findOne(query);
+            res.send(recipeDetails);
+        });
     }
     finally {
 
